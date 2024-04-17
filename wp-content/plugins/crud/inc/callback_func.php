40178908167a3668e4491db238a735651c02a6b6
afsar-uddin <afsarwebdev@gmail.com>
2024-04-17 06:53:48 +0600

    data insert to table crud

--- wp-content/plugins/crud/inc/callback_func.php
+++ wp-content/plugins/crud/inc/callback_func.php
@@ -5,25 +5,47 @@ function da_ems_list_callback() {
 }
 
 function da_ems_add_callback() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'crud';
+    $msg = '';
+
+    if (isset($_REQUEST['submit'])) {
+        $wpdb->insert("$table_name", [
+            "emp_id" => $_REQUEST['emp_id'],
+            'emp_name' => $_REQUEST['emp_name'],
+            'emp_email' => $_REQUEST['emp_email'],
+            'emp_dept' => $_REQUEST['emp_dept']
+        ]);
+
+        if ($wpdb->insert_id > 0) {
+            $msg = "Saved Successfully";
+        } else {
+            $msg = "Failed to save data";
+        }
+    }
+
     ?>
         <div class="wrap">
+            <h4 id="msg"><?php echo $msg; ?></h4>
             <form method="post">
-                <p>
-                    <label for="emp_id">EMP ID : </label>
-                    <input type="text" id="emp_id" placeholder="EMP Id" required>
-                </p>
-                <p>
-                    <label>Name</label>
-                    <input type="text" name="emp_name" placeholder="Enter Name" required>
-                </p>
-                <p>
-                    <label>Email</label>
-                    <input type="email" name="emp_email" placeholder="Enter Email" required>
-                </p>
-                <p>
-                    <label>Department</label>
-                    <input type="text" name="emp_dept" placeholder="Enter Department" required>
-                </p>
+                <table>
+                    <tr>
+                        <td><label for="emp_id">EMP ID : </label></td>
+                        <td><input type="text" id="emp_id" name="emp_id" placeholder="EMP Id" required></td>
+                    </tr>
+                    <tr>
+                        <td><label>Name : </label></td>
+                        <td><input type="text" name="emp_name" placeholder="Enter Name" required></td>
+                    </tr>
+                    <tr>
+                        <td><label>Email : </label></td>
+                        <td><input type="email" name="emp_email" placeholder="Enter Email" required></td>
+                    </tr>
+                    <tr>
+                        <td><label>Department : </label></td>
+                        <td><input type="text" name="emp_dept" placeholder="Enter Department" required></td>
+                    </tr>
+                </table>
                 <p>
                     <button type="submit" name="submit">Submit</button>
                 </p>
