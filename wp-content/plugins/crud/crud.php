d1345b0e82aee9c95e51109235ab5c0aa4833092
afsar-uddin <afsarwebdev@gmail.com>
2024-04-17 06:35:59 +0600

    file separeted by inc folder

--- wp-content/plugins/crud/crud.php
+++ wp-content/plugins/crud/crud.php
@@ -14,23 +14,6 @@ Text Domain: my-basics-plugin
 Domain Path: /languages 
 */
 
-register_activation_hook(file: __FILE__, callback: 'table_creator');
-
-function table_creator() {
-    global $wpdb;
-    $charset_collate = $wpdb->get_charset_collate();
-    $table_name = $wpdb->prefix.'crud';
-
-    $sql = "DROP TABLE IF EXIST $table_name; 
-            CREATE TABLE $table_name (
-                id mediumint(11) NOT NULL AUTO_INCREMENT,
-                emp_id varchar(50) NOT NULL,
-                emp_name varchar (250) NOT NULL,
-                emp_email varchar (250) NOT NULL,
-                emp_dept varchar (250) NOT NULL,
-                PRIMARY KEY id(id)
-            )$charset_collate;";
-    require_once (ABSPATH.'wp-admin/includes/upgrade.php');
-
-    dbDelta($sql);
+foreach ( glob( plugin_dir_path( __FILE__ ) . 'inc/*.php' ) as $file ) {
+    require_once $file;
 }
